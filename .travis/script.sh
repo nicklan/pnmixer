cc5ae4a18b7602e722325bba0cee208888fc19ba
hasufell <hasufell@hasufell.de>
2015-11-13 17:48:48 +0100

    Update travis CFLAGS

--- .travis/script.sh
+++ .travis/script.sh
@@ -10,21 +10,21 @@ case $OS in
 		docker run -ti \
 			-v "`pwd`":/pnmixer \
 			hasufell/gentoo-pnmixer-test:latest \
-			sh -c "cd /pnmixer && CC=$CC ./autogen.sh --enable-debug $BUILD_FLAGS && make" \
+			sh -c "cd /pnmixer && CC=$CC CFLAGS=\"$CFLAGS\" ./autogen.sh $BUILD_FLAGS && make" \
 			|| die "failed to build image on $OS"
 		;;
 	debian|Debian)
 		docker run -ti \
 			-v "`pwd`":/pnmixer \
 			hasufell/debian-pnmixer-test:latest \
-			sh -c "cd /pnmixer && CC=$CC ./autogen.sh --enable-debug $BUILD_FLAGS && make" \
+			sh -c "cd /pnmixer && CC=$CC CFLAGS=\"$CFLAGS\" ./autogen.sh $BUILD_FLAGS && make" \
 			|| die "failed to build image on $OS"
 		;;
 	ubuntu|Ubuntu)
 		docker run -ti \
 			-v "`pwd`":/pnmixer \
 			hasufell/ubuntu-pnmixer-test:latest \
-			sh -c "cd /pnmixer && CC=$CC ./autogen.sh --enable-debug $BUILD_FLAGS && make" \
+			sh -c "cd /pnmixer && CC=$CC CFLAGS=\"$CFLAGS\" ./autogen.sh $BUILD_FLAGS && make" \
 			|| die "failed to build image on $OS"
 		;;
 	*)
