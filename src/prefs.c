nicklan/pnmixer

commit 300409fddbda4e58889e3e626f7f6ba11f342a20

When card changes, do a full reinit to update icon

src/prefs.c
@@ -309,7 +309,7 @@ static void set_notification_options() {
 
 /**
  * Applies the preferences, usually triggered by on_ok_button_clicked()
- * in callbacks.c, but also initially caled from main().
+ * in callbacks.c, but also initially called from main().
  *
  * @param alsa_change whether we want to trigger alsa-reinitalization
  */
@@ -337,13 +337,14 @@ void apply_prefs(gint alsa_change) {
   set_notification_options();
 
   get_icon_theme();
-  if (alsa_change)
-    alsa_init();
+
   vol_meter_clrs = get_vol_meter_colors();
   set_vol_meter_color(vol_meter_clrs[0],vol_meter_clrs[1],vol_meter_clrs[2]);
   g_free(vol_meter_clrs);
   update_status_icons();
-  update_vol_text();
+
+  if (alsa_change)
+    do_alsa_reinit();
 }
 
 /**

src/alsa.c
@@ -248,8 +248,8 @@ static int alsa_cb(snd_mixer_elem_t *e, unsigned int mask) {
  * @return FALSE if the source should be removed,
  * TRUE otherwise
  */
-static gboolean idle_alsa_init(gpointer data) {
-  alsa_init();
+static gboolean idle_alsa_reinit(gpointer data) {
+  do_alsa_reinit();
   return FALSE;
 }
 
@@ -279,7 +279,7 @@ static gboolean poll_cb(GIOChannel *source,
      */
     do_notify_text(_("Soundcard disconnected"),
       _("Soundcard has been disconnected, reloading Alsa..."));
-    g_idle_add(idle_alsa_init, NULL);
+    g_idle_add(idle_alsa_reinit, NULL);
     return FALSE;
   }
   sread = 1;

commit 3a27ad8b08e92109453c3516855098c87f776022

Fix volume text setting that was ignored
Broken in commit 300409fddbda4e58889e3e626f7f6ba11f342a20,
that's quite old.

src/prefs.c
@@ -354,6 +354,7 @@ apply_prefs(gint alsa_change)
 			    vol_meter_clrs[2]);
 	g_free(vol_meter_clrs);
 	update_status_icons();
+	update_vol_text();
 
 	if (alsa_change)
 		do_alsa_reinit();