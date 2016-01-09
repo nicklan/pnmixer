9c2ab0bfa136ff59053ac77d0b054f4e5760e3ed
elboulangero <elboulangero@gmail.com>
2016-01-09 11:43:57 +0700

    Line up function names in prefs

--- src/prefs.h
+++ src/prefs.h
@@ -28,22 +28,21 @@ gint noti_timeout;
 GtkIconTheme *icon_theme;
 
 gboolean prefs_get_boolean(gchar *key, gboolean def);
-gint prefs_get_integer(gchar *key, gint def);
-gdouble prefs_get_double(gchar *key, gdouble def);
-gchar *prefs_get_string(gchar *key, const gchar *def);
-gchar *prefs_get_channel(const gchar *card);
-gchar *prefs_get_vol_command(void);
+gint     prefs_get_integer(gchar *key, gint def);
+gdouble  prefs_get_double(gchar *key, gdouble def);
+gchar   *prefs_get_string(gchar *key, const gchar *def);
+gchar   *prefs_get_channel(const gchar *card);
+gchar   *prefs_get_vol_command(void);
 #ifdef WITH_GTK3
 gdouble *prefs_get_vol_meter_colors(void);
 #else
-gint *prefs_get_vol_meter_colors(void);
+gint    *prefs_get_vol_meter_colors(void);
 #endif
 
 void prefs_set_boolean(const gchar *key, gboolean value);
 void prefs_set_integer(const gchar *key, gint value);
 void prefs_set_double(const gchar *key, gdouble value);
 void prefs_set_string(const gchar *key, const gchar *value);
-
 void prefs_set_channel(const gchar *card, const gchar *channel);
 #ifdef WITH_GTK3
 void prefs_set_vol_meter_colors(gdouble *colors, gsize n);
