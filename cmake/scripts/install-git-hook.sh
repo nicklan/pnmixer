3e9665d83e5465acc614485b3fe6ba5ac990a917
Julian Ospald <hasufell@posteo.de>
2017-03-12 18:38:55 +0100

    Fix git hook install script

--- cmake/scripts/install-git-hook.sh
+++ cmake/scripts/install-git-hook.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 if [ -f .githooks/pre-commit ] &&
+		[ -d .git ] &&
 		[ ! -f .git/hooks/pre-commit ]; then
 	cp -p .githooks/pre-commit .git/hooks/pre-commit &&
 	chmod +x .git/hooks/pre-commit &&
